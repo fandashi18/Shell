6bd2a7e06e638706f1ff1273a40b0132d279c189
fandashi18 <37918521+fandashi18@users.noreply.github.com>
2021-07-31 17:21:17 +0800

    Update update-hardware.sh

--- ubuntu20.04-config/update-hardware.sh
+++ ubuntu20.04-config/update-hardware.sh
@@ -3,3 +3,5 @@ sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
 ubuntu-drivers devices
 echo "\n 选择一个驱动，假设是a，现在输入命令 sudo apt install a\n然后重启系统"
+
+repo init -u git://mirrors.ustc.edu.cn/aosp/platform/manifest --depth=1 -b android-7.1.2_r39
